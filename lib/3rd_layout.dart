4fe79a7801e8a774ce2b681df35c2ad79fd3cec0
Vladimir <it_heyvlad@mail.ru>
2022-09-23 22:09:11 +0300

    Update 3rd_layout.dart

--- lib/3rd_layout.dart
+++ lib/3rd_layout.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class Tretiy extends StatelessWidget {
-  const Tretiy({super.key});
+class PlaylistsLayout extends StatelessWidget {
+  const PlaylistsLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
