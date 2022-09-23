1766dd7ef7af0c3b77e793035398746d858ae74b
Vladimir <it_heyvlad@mail.ru>
2022-09-23 22:09:51 +0300

    Update 4th_layout.dart

--- lib/4th_layout.dart
+++ lib/4th_layout.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class Chetvertiy extends StatelessWidget {
-  const Chetvertiy({super.key});
+class SignInLayout extends StatelessWidget {
+  const SignInLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
