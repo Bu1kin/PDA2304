8c96534d6dca0fe7076432f0b22e09b6dac6e29f
Vladimir <isip_v.i.malahov@mpt.ru>
2022-10-04 17:08:52 +0300

    добавили библиотеки, сделали создание таблиц Роль и Пользователь и многое другое

---
+++ lib/core/data_base_helper.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+import 'package:flutter_application_1/common/database_request.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+
+class DataBaseHelper{
+
+  static final DataBaseHelper instance = DataBaseHelper._instance();
+  DataBaseHelper._instance();
+
+  late final Directory _appDocumentDirectory;
+  late final String _pathDB;
+  late final Database dataBase;
+  final int _version = 1;
+
+  Future<void> init() async{
+    _appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+
+    _pathDB = join(_appDocumentDirectory.path, 'booksstore.db');
+
+    if(Platform.isLinux || Platform.isWindows || Platform.isMacOS){
+
+    }
+    else{
+      dataBase = await openDatabase(
+        _pathDB,
+        version: _version,
+        onCreate: (db, version) async {
+          await onCreateTable(db);
+        },
+      );
+    }
+  }
+
+
+  Future<void> onCreateTable(Database db)async{
+    for(var i = 0; i < DataBaseRequest.tableList.length; i++){
+      await db.execute(DataBaseRequest.tableCreateList[i]);
+    }
+  }
+}
