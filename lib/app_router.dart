cdcc55c359baf279fd7b3b2dc5556cce4f867c3e
Vladimir <it_heyvlad@mail.ru>
2023-03-05 21:06:53 +0300

    init commit

---
+++ lib/app_router.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/presentation/email_link.dart';
+import 'package:flutter_firebase/presentation/phone_number.dart';
+import 'package:flutter_firebase/presentation/signIn.dart';
+import 'package:flutter_firebase/presentation/signUp.dart';
+
+const String signInPage = 'signInPage';
+const String signUpPage = 'signUpPage';
+const String emailLinkPage = 'emailLinkPage';
+const String phoneNumber = 'phoneNumber';
+
+class AppRouter{
+  Route<dynamic>? generateRouter(RouteSettings settings){
+    switch(settings.name){
+      case signInPage :{
+        return MaterialPageRoute(builder: (builder) => SignIn());
+      }
+      case signUpPage :{
+        return MaterialPageRoute(builder: (builder) => SignUp());
+      }
+      case emailLinkPage :{
+        return MaterialPageRoute(builder: (builder) => EmailLinkSignIn());
+      }
+      case phoneNumber :{
+        return MaterialPageRoute(builder: (builder) => PhoneSignIn());
+      }
+    }
+  }
+}
